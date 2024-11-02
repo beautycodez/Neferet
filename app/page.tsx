717ef5f1611b746d468997bc4a18c2e001b89175
beautycodez <angellorl_94@hotmail.com>
2024-11-02 17:12:42 -0500

    sdfsd

--- app/page.tsx
+++ app/page.tsx
@@ -12,11 +12,8 @@ export default function Page() {
       <Hero />
       <main className="flex min-h-screen flex-col p-6">
         <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
-          <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
+          <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 w-screen ">
             <h1 className="text-gold text-2xl text-center">Categorías de Joyas</h1>
-          </div>
-          <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
-            {/* Add Hero Images Here */}
           </div>
         </div>
       </main>
