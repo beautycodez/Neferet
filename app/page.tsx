3a81eb2ea41801ae33bf4a800ee7c195942ea5ba
beautycodez <angellorl_94@hotmail.com>
2024-11-07 18:11:50 -0500

    sfsd

--- app/page.tsx
+++ app/page.tsx
@@ -10,15 +10,11 @@ export default function Page() {
     <>
       <Hero />
       <main className="flex min-h-screen flex-col p-1">
-        <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
-          <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 py-10 w-screen ">
-            <h1
-              className={`${lusitana.className} text-gold text-2xl text-center sm:text-sm`}
-            >
-              Categorías de Joyas
-            </h1>
-          </div>
-        </div>
+        <h1
+          className={`${lusitana.className} text-gold text-2xl text-center sm:text-sm`}
+        >
+          Categorías de Joyas
+        </h1>
       </main>
     </>
   );
