668161418213af523f6aae5d347afe8b0ecb0df5
beautycodez <angellorl_94@hotmail.com>
2025-05-03 17:18:50 -0500

    image issue fixed and enhanced, when deleting or updating the picture is remove from the database and cloudinary successfully. Additionally, an item can be updated without a file

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -3,9 +3,9 @@ import { z } from "zod";
 import { sql } from "@vercel/postgres";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
-import { v2 as cloudinary } from 'cloudinary';
-import { Readable } from 'stream';
-
+import { v2 as cloudinary } from "cloudinary";
+import { Readable } from "stream";
+import { fetchProductById } from "./data";
 // import { signIn } from '@/auth';
 // import { AuthError } from 'next-auth';
 
@@ -15,10 +15,9 @@ const FormSchema = z.object({
   categoriaId: z.string({
     invalid_type_error: "Please select a category.",
   }),
-  amount: z
-    .coerce
+  amount: z.coerce
     .number()
-    .gt(0, { message: "Please enter an amount greater than $0." }),  
+    .gt(0, { message: "Please enter an amount greater than $0." }),
   talla: z.coerce
     .number()
     .positive({ message: "Talla must be a positive number." })
@@ -27,18 +26,16 @@ const FormSchema = z.object({
     .number()
     .int({ message: "Cantidad must be an integer." })
     .positive({ message: "Cantidad must be a positive number." }),
-  descripcion: z
-    .string()
-    .min(1, { message: "Descripción is required." }),
-  nombre: z
-    .string()
-    .min(1, { message: "Nombre is required." }),
-    foto: z
+  descripcion: z.string().min(1, { message: "Descripción is required." }),
+  nombre: z.string().min(1, { message: "Nombre is required." }),
+  foto: z
     .instanceof(File)
     .refine(
       (file) => ["image/jpeg", "image/png", "image/gif"].includes(file.type),
       { message: "Foto must be a valid image file (JPEG, PNG, GIF)." }
     )
+    .optional(),
+  publicId: z.string().optional(), // Agrega esto si necesitas el publicId para eliminar la imagen
 });
 // Use Zod to update the expected types
 const UpdateInvoice = FormSchema.omit({ id: true });
@@ -54,9 +51,12 @@ export type State = {
     descripcion?: string[];
     nombre?: string[];
     foto?: string[];
+    
   };
   message?: string | null;
 };
+
+// Si no se proporciona una nueva foto, usa la foto existente
 
 export async function createProduct(prevState: State, formData: FormData) {
   // Validate form using Zod
@@ -67,11 +67,13 @@ export async function createProduct(prevState: State, formData: FormData) {
     cantidad: formData.get("cantidad"),
     descripcion: formData.get("descripcion"),
     nombre: formData.get("nombre"),
-    foto: formData.get("foto")
+    foto: formData.get("foto"),
   });
+  console.log("Validated fields:", validatedFields.data); // Log the validated fields
 
   // If form validation fails, return errors early. Otherwise, continue.
   if (!validatedFields.success) {
+    console.log("Validation errors:", validatedFields.error.flatten().fieldErrors);
     return {
       errors: validatedFields.error.flatten().fieldErrors,
       message: "Missing Fields. Failed to Create Invoice.",
@@ -79,33 +81,52 @@ export async function createProduct(prevState: State, formData: FormData) {
   }
 
   // Prepare data for insertion into the database
-  const { categoriaId, amount, talla, cantidad, descripcion, nombre, foto } = validatedFields.data;
+  const { categoriaId, amount, talla, cantidad, descripcion, nombre, foto } =
+    validatedFields.data;
   const amountInCents = amount * 100;
-  const fotoBase64 = await handleFileUpload (foto);
-
-  console.log(foto)
-
-
-  // Insert data into the database
-  try {
-    await sql`
-      INSERT INTO products (categoriaId, amount, talla, cantidad, descripcion, nombre, foto)
-      VALUES (${categoriaId}, ${amountInCents}, ${talla}, ${cantidad}, ${descripcion}, ${nombre}, ${fotoBase64})
+  let fotoBase64 = null; // Initialize fotoBase64 to null
+  if (foto) {
+    try {
+      fotoBase64 = await handleFileUpload(foto);
+      const publicId = fotoBase64.publicId; // Get the publicId from the upload result
+      // Insert data into the database
+      try {
+        await sql`
+      INSERT INTO products (categoriaId, amount, talla, cantidad, descripcion, nombre, foto, publicid)
+      VALUES (${categoriaId}, ${amountInCents}, ${talla}, ${cantidad}, ${descripcion}, ${nombre}, ${fotoBase64.secureUrl}, ${fotoBase64.publicId})
     `;
-  } catch (error) {
-    // If a database error occurs, return a more specific error.
+      } catch (error) {
+        // If a database error occurs, return a more specific error.
+        return {
+          message: `Database Error: Failed to Create Invoice and ${error}.`,
+        };
+      }
+
+      // Revalidate the cache for the invoices page and redirect the user.
+      revalidatePath("/admin");
+      revalidatePath("/products");
+      redirect("/products");
+    } catch (error) {
+      return {
+        errors: { foto: ["Failed to upload the image. Please try again."] },
+        message: "Image upload failed.",
+      };
+    }
+  } else {
     return {
-      message: `Database Error: Failed to Create Invoice and ${error}.`
+      errors: { foto: ["Image is required."] },
+      message: "Missing image. Please upload a valid image.",
     };
   }
-
-  // Revalidate the cache for the invoices page and redirect the user.
-  revalidatePath("/admin");
-  revalidatePath("/products");
-  redirect("/products");
 }
 
-export async function updateProduct(id: string, prevState: State, formData: FormData) {
+export async function updateProduct(
+  id: string,
+  prevState: State,
+  formData: FormData
+) {
+  const photo = formData.get("foto");//Se pasa un archivo File vacio lo cual hace que optional() no funcione, por eso agregamos algo de codigo extra para la validacion
+
   const validatedFields = UpdateInvoice.safeParse({
     categoriaId: formData.get("categoriaId"),
     amount: formData.get("amount"),
@@ -113,28 +134,53 @@ export async function updateProduct(id: string, prevState: State, formData: Form
     cantidad: formData.get("cantidad"),
     descripcion: formData.get("descripcion"),
     nombre: formData.get("nombre"),
-    foto: formData.get("foto")
+    foto: photo instanceof File && photo.size > 0 ? photo : undefined, // Ignora archivos vacíos
   });
- 
+
   if (!validatedFields.success) {
+    console.log("Validation errors:", validatedFields.error.flatten().fieldErrors);
     return {
       errors: validatedFields.error.flatten().fieldErrors,
-      message: 'Missing Fields. Failed to Update product.',
+      message: "Missing Fields. Failed to Update product.",
     };
   }
   // Prepare data for insertion into the database
-  const { categoriaId, amount, talla, cantidad, descripcion, nombre, foto } = validatedFields.data;
+  const { categoriaId, amount, talla, cantidad, descripcion, nombre, foto } =
+    validatedFields.data;
   const amountInCents = amount * 100;
-  const fotoBase64 = await handleFileUpload (foto);
 
-  try {
-    await sql`
-          UPDATE products
-          SET categoriaId = ${categoriaId}, amount = ${amountInCents}, talla = ${talla}, cantidad = ${cantidad}, descripcion = ${descripcion}, nombre = ${nombre}, foto = ${fotoBase64}
-          WHERE id = ${id}
-        `;
-  } catch (error) {
-    return { message: `Database Error: Failed to update Products and ${error}.` };
+  let fotoBase64 = null;
+  if (foto) {
+    const currentProduct = await fetchProductById(id);
+    deleteImgCloudinary(currentProduct.publicid); // Eliminar la imagen anterior de Cloudinary
+    // Subir la nueva imagen a Cloudinary
+    fotoBase64 = await handleFileUpload(foto);
+    try {
+      await sql`
+        UPDATE products
+        SET categoriaId = ${categoriaId}, amount = ${amountInCents}, talla = ${talla}, cantidad = ${cantidad}, descripcion = ${descripcion}, nombre = ${nombre}, foto = ${fotoBase64.secureUrl}, publicId = ${fotoBase64.publicId}
+        WHERE id = ${id}
+      `;
+    } catch (error) {
+      return {
+        message: `Database Error: Failed to update Products and ${error}.`,
+      };
+    }
+  } else {
+    // Obtén la foto existente de la base de datos
+    const existingProduct = await fetchProductById(id);
+    fotoBase64 = existingProduct.foto; // Usa la foto existente
+    try {
+      await sql`
+        UPDATE products
+        SET categoriaId = ${categoriaId}, amount = ${amountInCents}, talla = ${talla}, cantidad = ${cantidad}, descripcion = ${descripcion}, nombre = ${nombre}, foto = ${fotoBase64}, publicId = ${existingProduct.publicid}
+        WHERE id = ${id}
+      `;
+    } catch (error) {
+      return {
+        message: `Database Error: Failed to update Products and ${error}.`,
+      };
+    }
   }
 
   revalidatePath("/admin");
@@ -144,21 +190,28 @@ export async function updateProduct(id: string, prevState: State, formData: Form
 
 export async function deleteProduct(id: string) {
   try {
+    const product = await fetchProductById(id);
+    const publicId = product.publicid; // Obtén el publicId del producto
+    await deleteImgCloudinary(publicId); // Elimina la imagen de Cloudinary
     await sql`DELETE FROM products WHERE id = ${id}`;
     revalidatePath("/admin");
     revalidatePath("/products");
-    redirect("/products")
+    redirect("/products");
     return { message: "Deleted product." };
   } catch (error) {
-    return { message: `Database Error: Failed to delete product and ${error}.` };
+    return {
+      message: `Database Error: Failed to delete product and ${error}.`,
+    };
   }
 }
 
-async function handleFileUpload(file: File): Promise<string> {
+async function handleFileUpload(
+  file: File
+): Promise<{ secureUrl: string; publicId: string }> {
   return new Promise(async (resolve, reject) => {
     // Verificar si el archivo es una imagen válida
-    if (!file || !file.type.startsWith('image/')) {
-      return reject(new Error('Invalid file type'));
+    if (!file || !file.type.startsWith("image/")) {
+      return reject(new Error("Invalid file type"));
     }
 
     // Convertir el archivo a un buffer
@@ -166,19 +219,36 @@ async function handleFileUpload(file: File): Promise<string> {
     const fileStream = Readable.from(buffer); // Crear un Readable stream de Node.js a partir del buffer
 
     // Subir el archivo a Cloudinary utilizando el stream
-    cloudinary.uploader.upload_stream(
-      {
-        folder: 'productos', // Carpeta en Cloudinary donde se almacenará la imagen
-        use_filename: true, // Usar el nombre original del archivo
-        unique_filename: true, // No modificar el nombre del archivo
-      },
-      (error, result) => { // Simplificado para no usar los tipos específicos
-        if (error) {
-          reject(error); // Si hay error, rechaza la promesa
-        } else {
-          resolve(result?.secure_url ?? ''); // Retorna la URL segura de la imagen subida
+    cloudinary.uploader
+      .upload_stream(
+        {
+          folder: "productos", // Carpeta en Cloudinary donde se almacenará la imagen
+          use_filename: true, // Usar el nombre original del archivo
+          unique_filename: true, // No modificar el nombre del archivo
+        },
+        (error, result) => {
+          if (error) {
+            reject(error); // Si hay error, rechaza la promesa
+          } else {
+            resolve({
+              secureUrl: result?.secure_url ?? "", // Retorna la URL segura
+              publicId: result?.public_id ?? "", // Retorna el public_id
+            });
+          }
         }
+      )
+      .end(buffer); // Subir el buffer a Cloudinary
+  });
+}
+
+async function deleteImgCloudinary(publicId: string) {
+  return new Promise((resolve, reject) => {
+    cloudinary.uploader.destroy(publicId, (error, result) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(result);
       }
-    ).end(buffer); // Subir el buffer a Cloudinary
+    });
   });
 }
